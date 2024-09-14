0bcc1861b0043187a304cf6a881799913bc6df2c
mikepal2 <mikepal2@proton.me>
2024-09-13 19:33:01 -0700

    Fix sample name in comments

--- samples/sample-minimal/Program.cs
+++ samples/sample-minimal/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     // By using the [Command] attribute, we designate the Hello() method as the command handler.
     // Since this is the only method in the program with the [Command] attribute and the command name is not explicitly set in the attribute parameter,
-    // it automatically becomes the "Root" command. This means the method is executed by default, and no command name needs to be specified on the command line.
+    // it automatically becomes the root command. This means the method is executed by default, and no command name needs to be specified on the command line.
     // Additionally, any parameters of the method are treated as options by default, even if they are not explicitly declared with the [Option] attribute.
     [Command]
     public static void Hello(string name = "World")
@@ -17,7 +17,7 @@ internal class Program
 
 /* This program will produce output:
 
->sample-1-minimal.exe --help
+> sample-minimal.exe --help
 Description:
 
 Usage:
@@ -29,10 +29,10 @@ Options:
   -?, -h, --help  Show help and usage information
 
 
->sample-1-minimal.exe
+> sample-1-minimal.exe
 Hello World!
 
->sample-1-minimal.exe --name Joe
+> sample-minimal.exe --name Joe
 Hello Joe!
   
 */
