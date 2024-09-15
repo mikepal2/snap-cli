eb42ae60357c2e8a9e6c5b4bdf1e9e80d1861257
mikepal2 <mikepal2@proton.me>
2024-09-14 23:43:40 -0700

    Compatibility changes for latest System.CommandLine API

---
+++ src/ArgumentBuilder.cs
@@ -0,0 +1,53 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE-command-line-api.md file in the project root for full license information.
+
+using System;
+using System.CommandLine;
+using System.Reflection;
+
+internal static class ArgumentBuilder
+{
+    private static readonly ConstructorInfo _ctor;
+
+    static ArgumentBuilder()
+    {
+        _ctor = typeof(CliArgument<string>).GetConstructor(new[] { typeof(string) })!;
+    }
+
+    public static CliArgument CreateArgument(Type valueType, string name = "value")
+    {
+        var argumentType = typeof(CliArgument<>).MakeGenericType(valueType);
+
+#if NET6_0_OR_GREATER
+        var ctor = (ConstructorInfo)argumentType.GetMemberWithSameMetadataDefinitionAs(_ctor);
+#else
+        var ctor = argumentType.GetConstructor(new[] { typeof(string) });
+#endif
+
+        return (CliArgument)ctor.Invoke(new object[] { name });
+    }
+
+    internal static CliArgument CreateArgument(ParameterInfo argsParam)
+    {
+        if (!argsParam.HasDefaultValue)
+        {
+            return CreateArgument(argsParam.ParameterType, argsParam.Name!);
+        }
+
+        var argumentType = typeof(Bridge<>).MakeGenericType(argsParam.ParameterType);
+
+        var ctor = argumentType.GetConstructor(new[] { typeof(string), argsParam.ParameterType })!;
+
+        return (CliArgument)ctor.Invoke(new object[] { argsParam.Name!, argsParam.DefaultValue! });
+    }
+
+    private sealed class Bridge<T> : CliArgument<T>
+    {
+        public Bridge(string name, T defaultValue)
+            : base(name)
+        {
+            // this type exists only for an easy T => Func<ArgumentResult, T> transformation
+            DefaultValueFactory = (_) => defaultValue;
+        }
+    }
+}
