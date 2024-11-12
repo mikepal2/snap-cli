48b1e1a4444a51c9dbcfa788f74a7cf96aaefd09
mikepal2 <mikepal2@proton.me>
2024-11-12 15:30:18 -0800

    Move SnapCLI.DataAnnotations to separate repo

---
+++ tests/SnapCliUnitTest.cs
@@ -0,0 +1,158 @@
+﻿using SnapCLI;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tests
+{
+    public class SnapCliUnitTest : StaticFieldsCache
+    {
+        public static StringWriter Out = new StringWriter();
+
+        public enum UseExceptionHandler
+        {
+            Default,
+            Custom,
+            Null,
+            Throwing
+        }
+        public void TestCLI(string commandLine, string pattern, UseExceptionHandler useExceptionHandler = UseExceptionHandler.Default)
+        {
+            // synchronize tests
+            lock (Out)
+            {
+                var defaultExceptionHandler = CLI.ExceptionHandler;
+                try
+                {
+
+                    ResetValuesFromCache();
+
+                    switch (useExceptionHandler)
+                    {
+                        case UseExceptionHandler.Default: break;
+                        case UseExceptionHandler.Custom: CLI.ExceptionHandler = CustomExceptionHandler; break;
+                        case UseExceptionHandler.Null: CLI.ExceptionHandler = null; break;
+                        case UseExceptionHandler.Throwing: CLI.ExceptionHandler = ThrowingExceptionHandler; break;
+                        default: throw new ArgumentOutOfRangeException(nameof(useExceptionHandler));
+                    }
+
+                    try
+                    {
+                        int exitCode = CLI.Run(SplitArgs(commandLine).ToArray(), Out, Out);
+                        Out.WriteLine($"[exitCode:{exitCode}]");
+                    }
+                    catch (Exception ex)
+                    {
+                        Out.WriteLine($"[unhandled:{ex}]");
+
+                    }
+                    var output = Out.ToString().Trim(" \r\n".ToCharArray());
+                    bool expectedContains = true;
+
+                    if (pattern.StartsWith("!"))
+                    {
+                        expectedContains = false;
+                        pattern = pattern.Substring(1);
+                    }
+                    bool contains;
+                    if (pattern.StartsWith("like:"))
+                        contains = output.Like(pattern.Substring("like:".Length), substring: true);
+                    else if (pattern.StartsWith("regex:"))
+                        contains = Regex.IsMatch(output, pattern.Substring("regex:".Length));
+                    else
+                        contains = output.Contains(pattern);
+
+                    Assert.IsTrue(contains == expectedContains, $"Output {(expectedContains ? "does not contain" : "contains")} '{pattern}'\n\nOutput:\n{Out}\n");
+                }
+                finally
+                {
+                    // cleanup
+                    Out.GetStringBuilder().Clear();
+                    CLI.ExceptionHandler = defaultExceptionHandler;
+                }
+            }
+        }
+
+        // https://stackoverflow.com/a/64236441
+        public static IEnumerable<string> SplitArgs(string commandLine)
+        {
+            var result = new StringBuilder();
+
+            var quoted = false;
+            var escaped = false;
+            var started = false;
+            var allowcaret = false;
+            for (int i = 0; i < commandLine.Length; i++)
+            {
+                var chr = commandLine[i];
+
+                if (chr == '^' && !quoted)
+                {
+                    if (allowcaret)
+                    {
+                        result.Append(chr);
+                        started = true;
+                        escaped = false;
+                        allowcaret = false;
+                    }
+                    else if (i + 1 < commandLine.Length && commandLine[i + 1] == '^')
+                    {
+                        allowcaret = true;
+                    }
+                    else if (i + 1 == commandLine.Length)
+                    {
+                        result.Append(chr);
+                        started = true;
+                        escaped = false;
+                    }
+                }
+                else if (escaped)
+                {
+                    result.Append(chr);
+                    started = true;
+                    escaped = false;
+                }
+                else if (chr == '"')
+                {
+                    quoted = !quoted;
+                    started = true;
+                }
+                else if (chr == '\\' && i + 1 < commandLine.Length && commandLine[i + 1] == '"')
+                {
+                    escaped = true;
+                }
+                else if (chr == ' ' && !quoted)
+                {
+                    if (started) yield return result.ToString();
+                    result.Clear();
+                    started = false;
+                }
+                else
+                {
+                    result.Append(chr);
+                    started = true;
+                }
+            }
+
+            if (started) yield return result.ToString();
+        }
+
+        public static void TraceCommand(params object?[] args)
+        {
+            Out.WriteLine($"[{CLI.ParseResult.CommandResult.Command.FullName().Replace(' ', '_')}({string.Join(",", args)})]");
+        }
+
+        private static int CustomExceptionHandler(Exception exception)
+        {
+            Out.WriteLine($"[exception:{exception.Message}]");
+            return 999; // exit code
+        }
+
+        private static int ThrowingExceptionHandler(Exception exception)
+        {
+            Out.WriteLine($"[throw:{exception}]");
+            throw new Exception("Rethrowing", exception);
+        }
+    }
+
+
+}
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,28 +1,15 @@
 using SnapCLI;
 using System.CommandLine;
 using System.CommandLine.Builder;
-using System.Text;
-using System.Text.RegularExpressions;
-using SnapCLI.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
 
 [assembly: Command(Name = "level1cmd")]
 
 namespace Tests
 {
+
     [TestClass]
-    public class UnitTest1 : StaticFieldsCache
+    public class UnitTest1 : SnapCliUnitTest
     {
-        static StringWriter Out = new StringWriter();
-
-        public enum UseExceptionHandler
-        {
-            Default,
-            Custom,
-            Null,
-            Throwing
-        }
-
         [TestMethod]
         [DataRow("", "[testhost()]")]
         [DataRow("-?", "Root description")]
@@ -94,143 +81,6 @@ namespace Tests
         [DataRow("ann1 --opt1 11", "The option 'opt1' must be between 1 and 10")]
         [DataRow("ann1 --opt1 5 --opt2 aa", "minimum length of '3' and maximum length of '10'")]
         [DataRow("ann1 --opt1 5 --opt2 test!", "[ann1(5,test!)]")]
-        public void TestCLI(string commandLine, string pattern, UseExceptionHandler useExceptionHandler = UseExceptionHandler.Default)
-        {
-            // synchronize tests
-            lock (Out)
-            {
-                var defaultExceptionHandler = CLI.ExceptionHandler;
-                try
-                {
-
-                    ResetValuesFromCache();
-
-                    switch (useExceptionHandler)
-                    {
-                        case UseExceptionHandler.Default: break;
-                        case UseExceptionHandler.Custom: CLI.ExceptionHandler = CustomExceptionHandler; break;
-                        case UseExceptionHandler.Null: CLI.ExceptionHandler = null; break;
-                        case UseExceptionHandler.Throwing: CLI.ExceptionHandler = ThrowingExceptionHandler; break;
-                        default: throw new ArgumentOutOfRangeException(nameof(useExceptionHandler));
-                    }
-
-                    try
-                    {
-                        int exitCode = CLI.Run(SplitArgs(commandLine).ToArray(), Out, Out);
-                        Out.WriteLine($"[exitCode:{exitCode}]");
-                    }
-                    catch (Exception ex)
-                    {
-                        Out.WriteLine($"[unhandled:{ex}]");
-
-                    }
-                    var output = Out.ToString().Trim(" \r\n".ToCharArray());
-                    bool expectedContains = true;
-
-                    if (pattern.StartsWith("!"))
-                    {
-                        expectedContains = false;
-                        pattern = pattern.Substring(1);
-                    }
-                    bool contains;
-                    if (pattern.StartsWith("like:"))
-                        contains = output.Like(pattern.Substring("like:".Length), substring: true);
-                    else if (pattern.StartsWith("regex:"))
-                        contains = Regex.IsMatch(output, pattern.Substring("regex:".Length));
-                    else
-                        contains = output.Contains(pattern);
-
-                    Assert.IsTrue(contains == expectedContains, $"Output {(expectedContains ? "does not contain" : "contains")} '{pattern}'\n\nOutput:\n{Out}\n");
-                }
-                finally
-                {
-                    // cleanup
-                    Out.GetStringBuilder().Clear();
-                    CLI.ExceptionHandler = defaultExceptionHandler;
-                }
-            }
-        }
-
-        // https://stackoverflow.com/a/64236441
-        public static IEnumerable<string> SplitArgs(string commandLine)
-        {
-            var result = new StringBuilder();
-
-            var quoted = false;
-            var escaped = false;
-            var started = false;
-            var allowcaret = false;
-            for (int i = 0; i < commandLine.Length; i++)
-            {
-                var chr = commandLine[i];
-
-                if (chr == '^' && !quoted)
-                {
-                    if (allowcaret)
-                    {
-                        result.Append(chr);
-                        started = true;
-                        escaped = false;
-                        allowcaret = false;
-                    }
-                    else if (i + 1 < commandLine.Length && commandLine[i + 1] == '^')
-                    {
-                        allowcaret = true;
-                    }
-                    else if (i + 1 == commandLine.Length)
-                    {
-                        result.Append(chr);
-                        started = true;
-                        escaped = false;
-                    }
-                }
-                else if (escaped)
-                {
-                    result.Append(chr);
-                    started = true;
-                    escaped = false;
-                }
-                else if (chr == '"')
-                {
-                    quoted = !quoted;
-                    started = true;
-                }
-                else if (chr == '\\' && i + 1 < commandLine.Length && commandLine[i + 1] == '"')
-                {
-                    escaped = true;
-                }
-                else if (chr == ' ' && !quoted)
-                {
-                    if (started) yield return result.ToString();
-                    result.Clear();
-                    started = false;
-                }
-                else
-                {
-                    result.Append(chr);
-                    started = true;
-                }
-            }
-
-            if (started) yield return result.ToString();
-        }
-
-        private static void TraceCommand(params object?[] args)
-        {
-            Out.WriteLine($"[{CLI.ParseResult.CommandResult.Command.FullName().Replace(' ', '_')}({string.Join(",", args)})]");
-        }
-
-        private static int CustomExceptionHandler(Exception exception)
-        {
-            Out.WriteLine($"[exception:{exception.Message}]");
-            return 999; // exit code
-        }
-
-        private static int ThrowingExceptionHandler(Exception exception)
-        {
-            Out.WriteLine($"[throw:{exception}]");
-            throw new Exception("Rethrowing", exception);
-        }
 
 
         [Startup]
@@ -268,21 +118,6 @@ namespace Tests
                 return 1; // exit code
             };
         }
-
-#if BEFORE_AFTER_COMMAND_ATTRIBUTE
-        [BeforeCommand]
-        public static void PreCommand(ParseResult parseResult, Command command)
-        {
-            Out.WriteLine($"[PreCommand:{command.Name}]");
-        }
-
-
-        [AfterCommand]
-        public static void PostCommand(ParseResult parseResult, Command command)
-        {
-            Out.WriteLine($"[PostCommand:{command.Name}]");
-        }
-#endif
 
         [Option] // implicit Name= global-option-field
         public static string? globalOptionField = globalOptionFieldDefaultValue;
@@ -449,25 +284,6 @@ namespace Tests
                 );
         }
 
-
-        [Startup]
-        public static void Startup()
-        {
-            CLI.BeforeCommand += (args) => args.ParseResult.ValidateDataAnnotations();
-        }
-
-
-        [Command(Name = "ann1")]
-        public static void TestAnnotations1(
-            [Range(1, 10)]
-            int opt1 = 0,
-
-            [Length(3, 10)]
-            string opt2 = "test"
-            )
-        {
-            TraceCommand(opt1, opt2);
-        }
     }
 
 
