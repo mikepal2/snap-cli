f1f700a324c7723620c6e97364c5c6d9a49cf872
mikepal2 <mikepal2@proton.me>
2024-12-05 21:26:33 -0800

    Add more samples

--- samples/minimal/Program.cs
+++ samples/minimal/Program.cs
@@ -4,11 +4,10 @@
 
 internal class Program
 {
-    // By using the [Command] attribute, we designate the Hello() method as the command handler.
-    // Since this is the only method in the program with the [Command] attribute and the command name is not explicitly set in the attribute parameter,
-    // it automatically becomes the root command. This means the method is executed by default, and no command name needs to be specified on the command line.
-    // Additionally, any parameters of the method are treated as options by default, even if they are not explicitly declared with the [Option] attribute.
-    [Command]
+    // By using the [RootCommand] attribute, we designate the Hello() method as the root command handler. This means the method is executed by default,
+    // and no command name needs to be specified on the command line. Additionally, any parameters of the method are treated as options by default, even
+    // if they are not explicitly declared with the [Option] attribute.
+    [RootCommand]
     public static void Hello(string name = "World")
     {
         Console.WriteLine($"Hello {name}!");
---
+++ samples/simple-main/Program.cs
@@ -0,0 +1,48 @@
+﻿using SnapCLI;
+using System;
+
+// Simple CLI example with Main() method.
+//
+// Since there are no methods declared with the [Command] attribute, the SnapCLI library automatically binds the Main() method as the root command handler.
+// Any parameters of the Main() method are bound as options by default, even if they are not explicitly declared with the [Option] attribute,
+// while [Argument] attribute is used to declare a parameter to be bound as an argument. 
+//
+// This default behavior allows you to write simple CLI applications with minimal effort. See the documentation for more details.
+
+class Program
+{
+    static void Main(
+        [Argument] string name = "World",
+        ConsoleColor fgColor = ConsoleColor.White,
+        int repeat = 1)
+    {
+        Console.ForegroundColor = fgColor;
+        for (int i = 0; i < repeat; i++)
+            Console.WriteLine($"Hello {name}!");
+    }
+}
+
+/* This program generates following output
+   
+> simple-main -?
+Description:
+
+Usage:
+  simple-main [<name>] [options]
+
+Arguments:
+  <name>  [default: World]
+
+Options:
+  --fg-color <Black|Blue|Cyan|DarkBlue|DarkCyan|DarkGray|DarkGreen|DarkMagenta|DarkRed|DarkYellow|Gray|Green|Magenta|Red|White|Yellow>  [default: White]
+  --repeat <repeat>                                                                                                                     [default: 1]
+  --version                                                                                                                             Show version information
+  -?, -h, --help                                                                                                                        Show help and usage information
+
+> simple-main Joe --fg-color Yellow --repeat 2
+Hello Joe!
+Hello Joe!
+
+ */
+
+
